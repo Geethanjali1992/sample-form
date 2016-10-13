239082025c2db21dc609ba992f8b79b7bb1949f8
Geethanjali1992 <geethuguna@gmail.com>
2016-10-13 11:02:57 +0530

    Add files via upload

--- registration-form-validation.js
+++ registration-form-validation.js
@@ -1,167 +1,153 @@
-function formValidation()  
-{  
-var uname = document.registration.username.focus();  
-var umobile= document.registration.mobilenumber; 
-var udob=document.registration.dob;  
-var uage=document.registration.age; 
-var uadd = document.registration.address;  
-var ucity = document.registration.city;
-var uemail = document.registration.email;    
-var male = document.registration.male;  
-var female = document.registration.female;
-var gardening= document.registration.gardening;  
-var playingcricket = document.registration.playingcricket;
-var music = document.registration.music;  
-var reading = document.registration.reading;
-var cooking = document.registration.cooking;  
-return true;
-}
+
+function formValidation(){
+      var error = 0;
+      if(!username_validation('username')){
+        document.getElementById('usernameError').style.display = "block";
+        error++;
+      }
+     if(!mobilenumber_validation('mobilenumber')){
+        document.getElementById('mobilenumberError').style.display = "block";
+        error++;
+      }
+      if(!dob_validation('dob')){
+        document.getElementById('dobError').style.display = "block";
+        error++;
+      }
+      if(!age_validation('age')){
+        document.getElementById('ageError').style.display = "block";
+        error++;
+      }
+      if(!address_validation('address')){
+        document.getElementById('addressError').style.display = "block";
+        error++;
+      }
+       if(!city_validation('city')){
+        document.getElementById('cityError').style.display = "block";
+        error++;
+      }
+     if(!email_validation('email')){
+        document.getElementById('emailError').style.display = "block";
+        error++;
+      }
+       if(validate_gender('male')){
  
-function username_validation()  
-{   
-var uname = document.registration.username;
-var letters = /^[A-Za-z]+$/;  
-if(uname.value.match(letters))  
-{   
-return true;  
-}  
-else  
-{  
-document.getElementById("usernameError").innerHTML = "You must enter a username";
-document.getElementById("usernameError").style.color = "red";
-uname.focus();  
-return false;  
-}  
-}  
-function mobilenumber_validation()  
-{   
-var umobile= document.registration.mobilenumber; 
-var letters = /^[0-9]+$/;  
-if(umobile.value.match(letters))  
-{  
+      }else if(validate_gender('female')){
+         
+      }else{
+        document.getElementById('genderError').style.display = "block";
+        error++;
+      }
 
-return true;  
-}  
-else  
-{  
-alert('mobile number is invalid');  
-umobile.focus();  
-return false;  
-}  
-}  
-function dob_validation()  
-{   
-var udob=document.registration.dob;
-  if(udob.value=="")  
-{  
-alert('dob is required');  
-udob.focus();  
-return false;  
-}  
-else  
-{  
-return true;
-}  
-}  
-function age_validation()  
-{   
- var uage=document.registration.age; 
-  if(uage.value=="")  
-{  
-alert('age field is required');  
-uage.focus();  
-return false;  
-}  
-else  
-{  
-return true;
-}  
-}  
-function address_validation() 
-{   
-var uadd = document.registration.address;  
-if(uadd.value=="")  
-{  
-alert('address field is required');  
-uadd.focus();  
-return false;  
-}  
-else  
-{  
-return true;  
-}  
-}  
-function city_validation()  
-{  
-var ucity = document.registration.city;
-if(ucity.value == "Default")  
-{  
-alert('Select your city from the list');  
-ucity.focus();  
-return false;  
-}  
-else  
-{  
-return true;  
-}  
-}  
+     if(hobbiesvalidation('gardening')){
+ 
+      }else if(hobbiesvalidation('playingcricket')){
+         
+      }
+      else if(hobbiesvalidation('music')){
+         
+      }
+      else if(hobbiesvalidation('reading')){
+         
+      }
+      else if(hobbiesvalidation('cooking')){
+         
+      }
+     else{
+        document.getElementById('hobbiesError').style.display = "block";
+        error++;
+      }
+     
+      if(error > 0){
+        return false;
+      }
+    }  
+    function username_validation(x){
+      var re = /[A-Za-z ']$/;
+      if(re.test(document.getElementById(x).value)){
+        document.getElementById(x).style.background ='#ccffcc';
+        document.getElementById(x+'Error').style.display = "none";
+        return true;
+      }else{
+           document.getElementById(x).style.background ='#ff9999';
+        return false; 
+      }
+    }
+function mobilenumber_validation(x){
+       var re = /^[0-9]+$/;  
+       if(re.test(document.getElementById(x).value)){
+        document.getElementById(x).style.background ='#ccffcc';
+         document.getElementById(x+'Error').style.display = "none";
+        return true;
+      }else{
+         document.getElementById(x).style.background ='#ff9999';    
+            return false; 
+      }
+    }
+   function dob_validation(x){
+        if(document.getElementById(x).value){
+        document.getElementById(x).style.background ='#ccffcc';
+        document.getElementById(x+'Error').style.display = "none";
+        return true;
+      }else{
+         document.getElementById(x).style.background ='#ff9999';
+        return false; 
+      }
+    }
+ function age_validation(x){
+        if(document.getElementById(x).value){
+        document.getElementById(x).style.background ='#ccffcc';
+         document.getElementById(x+'Error').style.display = "none";
+        return true;
+      }else{
+           document.getElementById(x).style.background ='#ff9999';
+        return false; 
+      }
+    }
+   function address_validation(x){
+        if(document.getElementById(x).value){
+        document.getElementById(x).style.background ='#ccffcc';        
+        document.getElementById(x+'Error').style.display = "none";
+        return true;
+      }else{
+          document.getElementById(x).style.background ='#ff9999';
+         return false; 
+      }
+    }
+ function city_validation(x){
+      if(document.getElementById(x).value){
+        document.getElementById(x).style.background ='#ccffcc';
+        document.getElementById(x+'Error').style.display = "none";
+        return true;
+      }else{
+        
+        document.getElementById(x).style.background ='#ff9999';
+        return false; 
+      }
+    }
+function email_validation(x){
+     
+var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))+$/; 
+        if(re.test(document.getElementById(x).value)){
+        document.getElementById(x).style.background ='#ccffcc';
+        document.getElementById(x+'Error').style.display = "none";
+        return true;
+      }else{
+        document.getElementById(x).style.background ='#ff9999';
+        return false; 
+      }
+    }
 
-function email_validation()
-{  
-var uemail = document.registration.email;    
-var mailformat =/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))+$/; 
-if(uemail.value.match(mailformat))  
-{  
-return true; 
- }  
-else 
-{  
-alert("invalid email address");  
-uemail.focus();  
-return false;  
-} 
-}
-
-function submitt()
-{
-if(document.registration.username.value=="")
-{
-document.getElementById("usernameError").innerHTML = "You must enter a username";
-document.getElementById("usernameError").style.color = "red";
-}
-if(document.registration.mobilenumber.value=="")
-{
-document.getElementById("mobilenumberError").innerHTML = "You must enter a mobilenumber";
-document.getElementById("mobilenumberError").style.color = "red";
-}
-if(document.registration.dob.value=="")
-{
-document.getElementById("dobError").innerHTML = "You must enter a date of birth";
-document.getElementById("dobError").style.color = "red";
-}
-if(document.registration.age.value=="")
-{
-document.getElementById("ageError").innerHTML = "You must enter an age";
-document.getElementById("ageError").style.color = "red";
-}
-if(document.registration.address.value=="")
-{
-document.getElementById("addressError").innerHTML = "You must enter an address";
-document.getElementById("addressError").style.color = "red";
-}
-if(document.registration.city.value=="")
-{
-document.getElementById("cityError").innerHTML = "You must enter a city";
-document.getElementById("cityError").style.color = "red";
-}
-if(document.registration.email.value=="")
-{
-document.getElementById("emailError").innerHTML = "You must enter an email";
-document.getElementById("emailError").style.color = "red";
-}
-else
-{
-alert('Form submitted successfully');
-}
-}
+ function validate_gender(x){
+      if(document.getElementById(x).checked){
+    return true;
+      }else{
+     return false;
+      }
+    }
+    function hobbiesvalidation(x){
+      if(document.getElementById(x).checked){
+        return true;
+      }
+      return false;
+    }
 
